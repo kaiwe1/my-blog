87c514bc8c9dfcdeb27ba9f8962e5022d1b09c16
kaiwei <kaiwei.zh@qq.com>
2025-06-28 03:29:00 +0800

    feat: digital asset chart blueprint

--- src/app/apps/digital-asset-charts/page.tsx
+++ src/app/apps/digital-asset-charts/page.tsx
@@ -1,3 +1,9 @@
 export default function DigitalAssetChart() {
-    return <div>DigitalAssetChart</div>
+    return (
+        <div>
+            <div>DigitalAssetChart</div>
+            <div>VIX/Bitcoin and ETH price/CMC Greed and Fear Index/M2/USD Index/geopolitic news</div>
+            <div>rechart / tradingview / cmc</div>
+        </div>
+    )
 } 
