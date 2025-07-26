43c4a1d39370bfbeb64e4996cd25615d622d7701
kaiwei <kaiwei.zh@qq.com>
2025-07-26 21:06:51 +0800

    feat: 移除charts&sentry

--- src/app/charts/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function DigitalAssetChart() {
-    return (
-        <div>
-            <div>DigitalAssetChart</div>
-            <div>VIX/Bitcoin and ETH price/CMC Greed and Fear Index/M2/USD Index/geopolitic news</div>
-            <div>rechart / tradingview / cmc</div>
-        </div>
-    )
-} 
